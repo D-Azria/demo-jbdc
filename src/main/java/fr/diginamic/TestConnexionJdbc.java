340d38c6e077140821403b1fbeeac2074e16112f
D-Azria <d.azria.pro@gmail.com>
2023-06-12 15:07:13 +0200

    JBDC TP 02 ex1

--- src/main/java/fr/diginamic/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/TestConnexionJdbc.java
@@ -3,13 +3,24 @@ package fr.diginamic;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
-    private static final String dbUrl = "jdbc:mariadb://localhost:3306/COMPTA2";
-    private static final String dbUser = "root";
-    private static final String dbPW = "root";
+
+
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PW;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("database");
+        DB_URL = bundle.getString("database.url");
+        DB_USER = bundle.getString("database.user");
+        DB_PW = bundle.getString("database.pwd");
+
+    }
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPW)){
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PW)){
             System.out.println(connection);
         } catch (SQLException e){
             System.err.println(e.getMessage());
