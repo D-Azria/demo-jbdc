337319a32da7e65bfee78e92ba376bd13b4e7a47
D-Azria <d.azria.pro@gmail.com>
2023-06-13 10:11:37 +0200

    JBDC TP 03 ex3

---
+++ src/main/java/fr/diginamic/jbdc/TestDelete.java
@@ -0,0 +1,30 @@
+package fr.diginamic.jbdc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class TestDelete {
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PW;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("database");
+        DB_URL = bundle.getString("database.driver")+bundle.getString("database.dist_url")+":"+bundle.getString("database.dist_port")+"/"+bundle.getString("database.dist_name");
+        DB_USER = bundle.getString("database.dist_user");
+        DB_PW = bundle.getString("database.dist_pwd");
+    }
+    public static void main(String[] args) {
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
+             Statement monStatement = connection.createStatement()){
+            int nb = monStatement.executeUpdate("DELETE FROM FOURNISSEUR WHERE NOM='La Maison des Peintures'");
+            System.out.println(nb);
+        } catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
